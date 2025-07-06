d49e87de52903eb44eb10054a6a29945ff1c9fd7
saumyapandey1998 <saumyapandey98@gmail.com>
2025-07-06 19:25:26 -0400

    Implementing Dark Mode - Issue 4
    This patch used both local as database storage to store user preference for dark
    mode. Local storage is being used for anonymous users (users that are not logged in)
    Database storage used for authenticated users.
    CSS has been modified to improve readibility
    A minimal  button has been addded to the top right corner.
    
    I will add screenshots to the PR.

--- src/main/java/com/nadyagrishina/reflect_diary/secutiry/SecurityConfig.java
+++ src/main/java/com/nadyagrishina/reflect_diary/secutiry/SecurityConfig.java
@@ -38,6 +38,7 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.GET, "/auth/register").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
                         .requestMatchers("/css/**", "/fonts/**").permitAll()
+                        .requestMatchers("/js/**").permitAll()
                         .anyRequest().authenticated())
                 .formLogin(form -> form
                         .loginPage("/auth/login")
